ec4a908c7b8399a25473c28827e666f60dfdc999
ChipukaE <157606320+ChipukaE@users.noreply.github.com>
2024-11-19 09:26:22 +0200

    timezone update

---
+++ client/dist/assets/Error-420fd522.js
@@ -0,0 +1,1 @@
+import{c as r}from"./index-ae18f64d.js";const e=()=>r("div",{children:r("h1",{children:"Error"})});export{e as default};
--- client/dist/assets/Error-5e5f9e8d.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./index-6ed6c8e0.js";const e=()=>r("div",{children:r("h1",{children:"Error"})});export{e as default};
